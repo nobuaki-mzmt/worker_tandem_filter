43aa82cc6983f76c3afc3bc607f275d7b9eb3812
Nobuaki Mizumoto (水元 惟暁) <71577022+nobuaki-mzmt@users.noreply.github.com>
2026-04-19 12:09:44 -0500

    Update processing.R

--- code/processing.R
+++ code/processing.R
@@ -17,7 +17,7 @@
 }
 # ------------------------------------------------------------------------------
 
-# data prep
+# data prep ----
 {
   df_FM <-     arrow::read_feather("data_fmt/trajectory/FM_df.feather")
   df_alates <- arrow::read_feather("data_fmt/trajectory/alates_df.feather")
@@ -47,7 +47,7 @@
   df_soldier <- df_soldier %>% filter(!(video == "Ret_ama_SM_I_7-12"& ind_id %in% c(0, 3)))
 }
 
-# plot all trajectories
+# plot all trajectories ----
 {
   plot_traj <- function(df, ...){
     ggplot(df, aes(x = x_body, y = y_body, col = as.factor(ind_id) ))+
@@ -101,7 +101,7 @@
   }
 }
 
-# data pairing
+# data pairing ----
 {
   df_FM <- df_FM |> mutate(
     colony = str_split_i(video, "_", 4),
@@ -150,16 +150,366 @@
   
   save(df_all, df_body, file = "data_fmt/df_all.rda")
 }
+load("data_fmt/df_all.rda")
 
 df_pair <- df_all |> pivot_wider(
   id_cols = c(video, well_id, time_sec, colony, treat), 
   names_from = role_id,
   values_from = starts_with("x_") | starts_with("y_")
 )
+df_body <- df_body |> group_by(video, well_id) |> summarise(body_length = mean(body_length))
 
-df_all |> filter(video == "Ret_ama_SM_F_1-4", time_sec == 0.2) |> View()
+df_pair <- df_pair |> 
+  mutate(partner_dis = euclid_dis(x_body_0, y_body_0, x_body_1, y_body_1)) |>
+  mutate(follow_dis = euclid_dis(x_tip_0, y_tip_0, x_head_1, y_head_1)) |>
+  left_join(df_body, by = c("video", "well_id")) |>
+  mutate(partner_dis = partner_dis / body_length) |>
+  mutate(follow_dis = follow_dis / body_length) |>
+  mutate(
+    ang_0 = atan2(y_head_0 - y_tip_0, x_head_0 - x_tip_0),
+    ang_1 = atan2(y_head_1 - y_tip_1, x_head_1 - x_tip_1),
+    ang_1to0 = atan2(y_body_0 - y_body_1, x_body_0 - x_body_1),
+    ang_0to1 = ang_1to0 + pi
+  ) |>
+  mutate(
+    dir_1to0 = ang_1to0 - ang_1,
+    dir_0to1 = ang_0to1 - ang_0
+  ) |>
+  dplyr::select(video, well_id, time_sec, colony, treat, 
+                partner_dis, follow_dis, dir_0to1, dir_1to0)
+
+df_rel <- df_pair |>
+  mutate(rx0 = partner_dis * cos(dir_1to0),
+         ry0 = partner_dis * sin(dir_1to0),
+         rx1 = partner_dis * cos(dir_0to1),
+         ry1 = partner_dis * sin(dir_0to1))
+
+video_list <- unique(df_rel$video )
+
+for(i_v in video_list){
+  df_temp <- df_rel |> filter(video == i_v)
+  p1 <- ggplot(df_temp, aes(x = rx0, y = ry0)) +
+    geom_bin_2d(binwidth = 0.1) +
+    facet_wrap(~well_id) +
+    scale_fill_viridis() +
+    coord_cartesian(xlim = c(-2,2), ylim = c(-2,2)) +
+    theme(legend.position = "none", aspect.ratio = 1) +
+    labs(x = "", y = "", title = i_v)
+  ggsave(p1, file = sprintf("output/relative_pos/%s.pdf", i_v), width = 4, height = 3)
+}
+
+# ----
+calc_rel_geometry <- function(mx, my, tx, ty, dir_x, dir_y) {
+  rel_x <- mx - tx
+  rel_y <- my - ty
+  list(
+    ang = atan2(rel_y, rel_x) - atan2(dir_y, dir_x) + pi/2,
+    dis = sqrt(rel_x^2 + rel_y^2)
+  )
+}
 
 
+
+df_tandem_time <- df_pair |> 
+  mutate(tandem = follow_dis < 1,
+         time_bin = round(time_sec)) |>
+  group_by(time_bin, treat) |>
+  summarize(tandem_prop = mean(tandem)) |> ungroup()
+
+ggplot(df_tandem_time, aes(x = time_bin, y = tandem_prop, 
+                           group = treat, col = treat)) +
+  geom_line(alpha=0.5)
+
+library(ggridges)
+
+ggplot(df_pair, aes(x = follow_dis, y = treat)) +
+  geom_density_ridges()
+  
+
+
+df_tandem <- df_pair |>
+  group_by(video, well_id) |>
+  arrange(time_sec, .by_group = TRUE) |>
+  mutate(
+    tandem = follow_dis < 2,
+    cluster_start = tandem & (lag(!tandem, default = TRUE)),
+    cluster_idx = cumsum(cluster_start),
+    cluster_idx = if_else(tandem, cluster_idx, NA_integer_),
+    pair_event = if_else(
+      !is.na(cluster_idx),
+      sprintf("%s_%d_%02d", video, well_id, cluster_idx),
+      NA_character_
+    )
+  ) |>
+  dplyr::select(-cluster_start, -cluster_idx) |>
+  ungroup()
+
+
+
+df_pair_dur <- df_tandem |> filter(time_sec < 1800.1) |>
+  filter(!is.na(pair_event)) |>
+  group_by(pair_event, treat, well_id, video) |>
+  summarise(
+    duration = (n() * 0.2), 
+    cens = !((last(time_sec) == 1800) | (first(time_sec) == 0)) ,
+    .groups = "drop"
+  )
+
+library(survminer)    
+library(survival)
+
+library(scales)
+
+ggplot(df_pair_dur, aes(x = treat, y = duration) ) +
+  geom_point()
+
+df_plot <- df_pair_dur |> filter(duration > 0) |>
+  filter(treat != "FW")
+
+ggsurvplot(
+  survfit(Surv(duration, cens) ~ treat, data = df_plot),
+  data = df_plot,
+  censor = FALSE,
+  conf.int.style = "ribbon",
+  conf.int.fill = TRUE,
+  ggtheme = theme_classic(),
+  
+)$plot + 
+  scale_x_continuous(trans = "pseudo_log", breaks = c(0, 1, 10, 100, 1000))
+
+#cumulative hazard plot
+ggsurvplot(
+  survfit(Surv(duration, cens) ~ treat, data = df_plot),
+  data = df_plot,
+  fun = "cumhaz",
+  censor = TRUE,
+  ggtheme = theme_classic()
+)$plot
+
+
+df_split <- survSplit(
+  Surv(duration, cens) ~ ., 
+  data = df_plot,
+  cut = c(200, 600),
+  episode = "phase"
+)
+
+r <- coxph(Surv(tstart, duration, cens) ~ treat * phase, data = df_split)
+summary(r)
+
+
+fit_weibull <- survreg(Surv(duration, cens) ~ treat, data = df_plot, dist = "weibull")
+summary(fit_weibull)
+
+shape <- 1 / fit_weibull$scale
+shape
+
+library(flexsurv)
+
+fit <- flexsurvreg(Surv(duration, cens) ~ treat, data = df_plot, dist = "weibull")
+fit
+
+# estimate hazard (smoothed)
+library(muhaz)
+hz <- muhaz(df$time, df$event)
+plot(hz)
+
+
+df_pair_dur |> group_by(treat, video, well_id) |>
+  summarise(duration = max(duration)) |>
+  ggplot(aes(y = treat, x = duration)) +
+  geom_jitter(height = 0.2)
+
+
+####
+
+gap_max <- 10  # number of frames allowed as gap (e.g., 1 sec)
+
+min_len <- 10  # minimum frames to count as tandem (e.g., 1 sec)
+
+df_tandem <- df_pair |>
+  group_by(video, well_id) |>
+  arrange(time_sec, .by_group = TRUE) |>
+  mutate(
+    tandem_raw = partner_dis < 2,
+    
+    # identify runs
+    run_id = rleid(tandem_raw),
+    
+    # run length
+    run_len = ave(tandem_raw, run_id, FUN = length)
+  ) |>
+  
+  # keep only sufficiently long tandem runs
+  mutate(
+    tandem = if_else(tandem_raw & run_len >= min_len, TRUE, FALSE)
+  ) |>
+  
+  # define clusters
+  mutate(
+    cluster_start = tandem & lag(!tandem, default = TRUE),
+    cluster_idx = cumsum(cluster_start),
+    cluster_idx = if_else(tandem, cluster_idx, NA_integer_),
+    pair_event = if_else(
+      !is.na(cluster_idx),
+      sprintf("%s_%d_%02d", video, well_id, cluster_idx),
+      NA_character_
+    )
+  ) |>
+  select(-cluster_start, -cluster_idx, -run_id, -run_len, -tandem_raw) |>
+  ungroup()
+
+df_pair_dur <- df_tandem |> filter(time_sec < 1800.1) |>
+  filter(!is.na(pair_event)) |>
+  group_by(pair_event, treat, well_id, video) |>
+  summarise(
+    duration = (n() * 0.2), 
+    cens = !(dplyr::near(last(time_sec), 1800) | dplyr::near(first(time_sec), 0)),
+    .groups = "drop"
+  )
+
+
+
+df_plot <- df_pair_dur |> filter(duration > 0) |>
+  filter(treat != "FW")
+
+ggsurvplot(
+  survfit(Surv(duration, cens) ~ treat, data = df_plot),
+  data = df_plot,
+  censor = FALSE,
+  conf.int.style = "ribbon",
+  conf.int.fill = TRUE,
+  ggtheme = theme_classic(),
+  
+)$plot + 
+  scale_x_continuous(trans = "pseudo_log", breaks = c(0, 1, 10, 100, 1000))
+
+#cumulative hazard plot
+ggsurvplot(
+  survfit(Surv(duration, cens) ~ treat, data = df_plot),
+  data = df_plot,
+  fun = "cumhaz",
+  censor = TRUE,
+  ggtheme = theme_classic()
+)$plot
+
+
+
+df_pair_dur |>
+  count(video, well_id, treat) |>
+  ggplot(aes(x = n, y = treat)) + geom_density_ridges(stat = "binline")
+
+df_pair_dur |>
+  count(video, well_id, treat) |>
+  ggplot(aes(x = n)) + geom_histogram()
+
+
+df_pair_dur |>
+  group_by(video, well_id, treat) |>
+  summarise(n_events = n(), total_time = sum(duration)) |>
+  arrange(desc(n_events))
+
+df_pair_dur <- df_pair_dur |>
+  group_by(video, well_id) |>
+  mutate(weight = 1 / n()) |>
+  ungroup()
+
+coxph(Surv(duration, cens) ~ treat, data = df_pair_dur, weights = weight)
+
+
+df_plot <- df_pair_dur |> filter(treat != "FW") |>
+  group_by(video, well_id, treat) |>
+  filter(duration == max(duration))
+
+#cumulative hazard plot
+ggsurvplot(
+  survfit(Surv(duration, cens) ~ treat, data = df_plot),
+  data = df_plot,
+  fun = "cumhaz",
+  censor = TRUE,
+  ggtheme = theme_classic()
+)$plot
+
+
+
+##
+df_pair_dur <- df_tandem |> filter(time_sec < 1800.1) |>
+  filter(!is.na(pair_event)) |> filter(treat != "FW") |>
+  group_by(pair_event, treat, well_id, video) |>
+  summarise(
+    start_time = first(time_sec),
+    duration = (n() * 0.2), 
+    cens = !(dplyr::near(last(time_sec), 1800) | dplyr::near(first(time_sec), 0)),
+    .groups = "drop"
+  ) |>
+  group_by(video, well_id) |>
+  mutate(event_order = row_number())
+
+
+df_plot <- df_pair_dur |> filter(treat != "FW") |>
+  group_by(video, well_id, treat) |>
+  filter(duration == max(duration))
+
+r <- flexsurvreg(Surv(duration, cens) ~ treat, data = df_plot, dist = "weibull")
+
+fit_FM  <- flexsurvreg(Surv(duration, cens) ~ 1, data = subset(df_pair_dur, treat == "FM"), dist = "weibull")
+fit_MW  <- flexsurvreg(Surv(duration, cens) ~ 1, data = subset(df_pair_dur, treat == "MW"), dist = "weibull")
+fit_MW2 <- flexsurvreg(Surv(duration, cens) ~ 1, data = subset(df_pair_dur, treat == "MW2"), dist = "weibull")
+fit_SM  <- flexsurvreg(Surv(duration, cens) ~ 1, data = subset(df_pair_dur, treat == "SM"), dist = "weibull")
+sapply(list(FW=fit_FM, MW=fit_MW, MW2=fit_MW2, SM=fit_SM),
+       function(x) x$res["shape","est"])
+
+
+
+ggplot(df_pair_dur, aes(x = event_order, y =duration)) + geom_point() + facet_wrap(~treat)
+
+df_plot <- df_pair_dur |> filter(event_order < 3)
+
+#cumulative hazard plot
+ggsurvplot(
+  survfit(Surv(duration, cens) ~ treat , data = df_plot),
+  data = df_plot,
+  fun = "cumhaz",
+  censor = TRUE,
+  color = "treat",
+  ggtheme = theme_classic()
+)$plot
+
+
+df_plot <- df_pair_dur |> mutate(early = start_time < 300)
+
+#cumulative hazard plot
+ggsurvplot(
+  survfit(Surv(duration, cens) ~ treat + early, data = df_plot),
+  data = df_plot,
+  fun = "cumhaz",
+  censor = TRUE,
+  color = "treat",
+  ggtheme = theme_classic()
+)$plot +facet_wrap(~early) 
+
+
+
+
+
++
+  scale_x_continuous(breaks = seq(0, 900, 450), limits = c(0, 900)) +
+  scale_y_continuous(breaks = seq(0, 1, 0.5), labels = c(0,0.5,1)) +
+  scale_color_manual(values = box_colors, labels = c("Male absent", "Male present")) +
+  scale_fill_manual(values = box_colors) +
+  theme(
+    legend.position = c(0.35, 0.15),
+    legend.direction = "horizontal",  
+    legend.box = "vertical",
+    #legend.justification = c("right", "bottom"),
+    legend.title = element_blank(),
+    legend.text = element_text(size = 9),
+    axis.title.x = element_text(size = 11),
+    axis.title.y = element_text(size = 11),
+    axis.text = element_text(size = 9),
+    aspect.ratio =3/4
+  ) +
+  guides(fill = "none")
 
 
 # ------------------------------------------------------------------------------
